7314c7ded2ab7a89269e3cde9e5ca46cf867f596
James Kent <107423859+jameskentdev@users.noreply.github.com>
2023-12-31 16:19:38 +0100

    finish Follow Along: Phoenix Counter App exercise

---
+++ projects/counter/lib/counter_web/controllers/counter_controller.ex
@@ -0,0 +1,17 @@
+defmodule CounterWeb.CounterController do
+  use CounterWeb, :controller
+
+  def count(conn, params) do
+    # The template atom is used to find a template used to render the page. In this case it's an atom, so I need provide
+    # a page which contains that atom; "counter_html/count.html.heex".
+    # The path starts from "counter_html" because that is what is set in counter_html.ex.
+    render(conn, :count, count: params["count"] || 0)
+  end
+
+  def increment(conn, params) do
+    IO.puts(" in increment")
+    current_count = String.to_integer(params["count"])
+    increment_by = String.to_integer(params["increment_by"])
+    render(conn, :count, count: current_count + increment_by)
+  end
+end
