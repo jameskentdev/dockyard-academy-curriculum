a5929ebaf39e0b8d91d394bd3a92168e37f205b8
James Kent <107423859+jameskentdev@users.noreply.github.com>
2023-12-31 16:58:18 +0100

    finish Follow Along: Phoenix Counter App exercise

--- projects/counter/lib/counter_web/controllers/counter_controller.ex
+++ projects/counter/lib/counter_web/controllers/counter_controller.ex
@@ -8,6 +8,7 @@ defmodule CounterWeb.CounterController do
     render(conn, :count, count: params["count"] || 0)
   end
 
+  @spec increment(Plug.Conn.t(), nil | maybe_improper_list() | map()) :: Plug.Conn.t()
   def increment(conn, params) do
     IO.puts(" in increment")
     current_count = String.to_integer(params["count"])
